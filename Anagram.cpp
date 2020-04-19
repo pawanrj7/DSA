948198d7392e0581d995664eec29b7d0a2a5efc3
pawanrj7 <pr.pawanrj7@gmail.com>
2020-04-20 02:11:07 +0530

    Anagram code is fixed

--- Anagram.cpp
+++ Anagram.cpp
@@ -1,13 +1,50 @@
-#include<iostream.h>
-#include<string.h>
+#include <iostream>
+using namespace std;
+
+#include <string.h>
+
+int  isAna(int hash[])
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (hash[i] != 0)
+            {
+                return 1;
+            }
+            
+        }
+        return 0;
+    }
 
 int main()
 {
-    char a[100],b[100];
-    cout<<"Enter the first string:\n";
+    char a[100], b[100],result;
+    cout << "Enter the first string:\n";
     gets(a);
-    cout<<"Enter the second string:\n";
+    cout << "Enter the second string:\n";
     gets(b);
 
+    int hash[26] = {0};
+    for (int i = 0; i < strlen(a); i++)
+    {
+        hash[a[i]-97] += 1;
+    }
 
+    for (int i = 0; i < strlen(b); i++)
+    {
+        hash[b[i]-97] -= 1;
+    }
+    result= isAna(hash);
+
+    if(result==1)
+    {
+        cout<<"Strings are not Anagram";
+    }
+    else
+    {
+        cout<<"Strings are Anagram";
+    }
+
+    return 0;
+    
 }
