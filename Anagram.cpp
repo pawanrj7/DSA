522d64badd55e2d5196f689e3a6614c02e45de09
pawanrj7 <pr.pawanrj7@gmail.com>
2020-04-19 22:55:12 +0530

    Anagram code is added

---
+++ Anagram.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<string.h>
+
+int main()
+{
+    char a[100],b[100];
+    cout<<"Enter the first string:\n";
+    gets(a);
+    cout<<"Enter the second string:\n";
+    gets(b);
+
+
+}
